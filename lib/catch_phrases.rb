9f49f3e505c1b60566be5ef0efd675176b92ef53
Laura Archambault <laarchambault@gmail.com>
2019-10-06 18:46:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def mario
-
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -8,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def any_phrase(phrase)
+  puts phrase
+end
+
